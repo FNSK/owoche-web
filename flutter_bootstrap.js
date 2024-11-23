ed8a0d8bb483999e3273c1e49478f12ab3abe2e9
Karl FNSK <fnskarl@gmail.com>
2024-11-23 10:47:20 +0100

    Fixed services screens for mobile

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "328176316"
+    serviceWorkerVersion: "1358844514"
   }
 });
