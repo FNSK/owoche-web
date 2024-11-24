caa46a06c1bb72f7f2d6484fa60a828779e13422
Karl FNSK <fnskarl@gmail.com>
2024-11-24 07:20:46 +0100

    Fixed contact picker for web and logo asset issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3666456203"
+    serviceWorkerVersion: "2537272168"
   }
 });
