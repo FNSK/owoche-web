46b640f056e755edaf625f5de6ed6864e622a28b
Karl FNSK <fnskarl@gmail.com>
2024-11-25 13:02:45 +0100

    Added splash screens, workflow logic from startup to dashboard (services)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,14 +42,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d591777723bf38645262e061a54f615f
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "1cbfacd0f1fd58576e855e90d49ad880",
+"flutter_bootstrap.js": "0b590553a8e305288ac5695a7ef91d2f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9f958814e7e66272f89bf4576c7f6bc8",
-"/": "9f958814e7e66272f89bf4576c7f6bc8",
-"main.dart.js": "70bbca36b2728988b8b1f80afc278f18",
+"index.html": "14b711e8396df28897944bf66390ce2d",
+"/": "14b711e8396df28897944bf66390ce2d",
+"main.dart.js": "de68fd714cdc14fe6d7b9c65bc14dd62",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can
